3b1e13ced4dbf80dc4ff5284c751ab244a5476cc
kemalloncel <kemaloncel3535@outlook.com>
2024-10-10 11:45:34 +0300

    create workspace

---
+++ src/api/allergy/routes/allergy.ts
@@ -0,0 +1,7 @@
+/**
+ * allergy router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::allergy.allergy');
---
+++ src/api/allergy/services/allergy.ts
@@ -0,0 +1,7 @@
+/**
+ * allergy service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::allergy.allergy');
